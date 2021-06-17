82dc2993171f75babd6c750843ed2b2e7d221662
bulentbariskilic@gmail.com <bulentbariskilic@gmail.com>
2021-06-17 23:54:22 +0300

    typeConversions added.

---
+++ 04-typeConversions/src/main/kotlin/main.kt
@@ -0,0 +1,14 @@
+fun main(args: Array<String>) {
+    var number1 : Int = 5
+    var number2 : Long = 122
+
+    var result : Long = number1.toLong() * number2
+    println(result)
+
+    println("20.607 to int: " + (20.607.toInt()))
+
+    val number3 : Long = 4324234344
+    var number4 : Int = number3.toInt()
+    println(number3)
+    println(number4)
+}
