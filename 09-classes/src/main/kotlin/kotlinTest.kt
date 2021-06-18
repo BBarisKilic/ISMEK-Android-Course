7d0fe99b09783d50819ef912a54f9e6d8689ca59
bulentbariskilic@gmail.com <bulentbariskilic@gmail.com>
2021-06-18 15:10:19 +0300

    classes added.

---
+++ 09-classes/src/main/kotlin/kotlinTest.kt
@@ -0,0 +1,9 @@
+class kotlinTest {
+    private var name : String = "ISMEK Android"
+
+    fun test() {
+        println("Called test function: $name")
+    }
+
+
+}
---
+++ 09-classes/src/main/kotlin/main.kt
@@ -0,0 +1,4 @@
+fun main(args: Array<String>) {
+    val myClass = kotlinTest()
+    myClass.test()
+}
