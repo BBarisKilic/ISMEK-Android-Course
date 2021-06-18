bb7c04b5ae0b3b5e92c87a8aa545f23b72afa446
bulentbariskilic@gmail.com <bulentbariskilic@gmail.com>
2021-06-18 12:11:56 +0300

    valAndVar added.

---
+++ 05-valAndVar/src/main/kotlin/main.kt
@@ -0,0 +1,11 @@
+fun main(args: Array<String>) {
+    val name = "Baris"
+    println(name)
+
+    //name = "Kotlin"
+    println(name)
+
+    var name2 = "Baris"
+    name2 = "Kotlin"
+    println(name2)
+}
